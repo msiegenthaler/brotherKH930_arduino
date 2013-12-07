66b9cc8af600e657aad4c4ab604310b958889212
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-07 21:33:17 +0100

    Add pattern to solenoids and do some cleanup.

--- solenoids.cpp
+++ solenoids.cpp
@@ -1,15 +1,16 @@
 #include "solenoids.h"
 
-Solenoids::Solenoids(int pinBP, int solenoidPins[]) {
+Solenoids::Solenoids(int pinBP, const int solenoidPins[]) {
   this->pinBP = pinBP;
   pinMode(pinBP, INPUT);
   for (int i=0; i<16; i++) {
-    this->pins[i] = solenoidPins[i];
+    this->pin[i] = solenoidPins[i];
     pinMode(solenoidPins[i], INPUT);
   }
+  for (int i=0; i<200; i++) pattern[i] = false;
 }
 
-void Solenoids::onTurnMark(boolean is_L_carriage, boolean left) {
+void Solenoids::onTurnmark(boolean is_L_carriage, boolean left) {
   // See the table on page 7 in the support manual
   boolean bp = digitalRead(pinBP) == HIGH;
   if (is_L_carriage) mode2 = !bp; //L and anywhere
@@ -22,4 +23,8 @@ int Solenoids::needleToSoleniod(int needle) {
   int result = (needle + 100) % 16;
   if (mode2) result = (result + 8) % 16;
   return result;
+}
+
+void Solenoids::needle(int needle, boolean value) {
+  pattern[needle] = value;
 }
--- solenoids.h
+++ solenoids.h
@@ -5,13 +5,19 @@
 
 /**
  * Solenoids can pull down a needle so it gets moved into D position.
- * Needles: Left = -100 until -1; Right = 0 until 99.
  */
 class Solenoids {
-  Solenoids(int pinBP, int solenoidPins[]);
+public:
+  Solenoids(int pinBP, const int solenoidPins[]);
 
   /** Call whenever the carriage passes the left or right turnmark. */
-  void onTurnMark(boolean is_L_carriage, boolean left);
+  void onTurnmark(boolean is_L_carriage, boolean left);
+
+  /**
+   * Set the value of a needle. True means move to position B, false to position D.
+   * Needles: Left = -100 until -1; Right = 0 until 99.
+   */
+  void needle(int needle, boolean value);
 
 private:
   int needleToSoleniod(int needle);
@@ -19,6 +25,7 @@ private:
   int pinBP;
   int pin[16]; //0 until F
   boolean mode2; //mode 2 = right column in support manual
+  boolean pattern[200];
 };
 
 #endif
