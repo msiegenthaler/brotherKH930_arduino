246293da198aff27756ccd14d394e8469b7e631d
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-08 20:20:43 +0100

    Better output (use tabs and also report carriage position.

--- brotherKH930_arduino.ino
+++ brotherKH930_arduino.ino
@@ -19,21 +19,25 @@ void loop() {
   if (changed) {
     changed = false;
 
-    Direction dir = brother.direction();
-    int pos = brother.needle();
-
     Serial.print("@");
-    Serial.print(pos);
-    Serial.print("  ");
+    Serial.print(brother.needle());
+    Serial.print("\t");
 
-    if (dir == LEFT) Serial.print("<-");
+    if (brother.direction() == LEFT) Serial.print("<-");
     else Serial.print("->");
-    Serial.print(" ");
+    Serial.print("\t");
 
     if (brother.carriageType() == K_CARRIAGE) Serial.print("K");
     else if (brother.carriageType() == G_CARRIAGE) Serial.print("G");
     else if (brother.carriageType() == L_CARRIAGE) Serial.print("L");
-    else Serial.print("Unknown");
+    else Serial.print("?");
+    Serial.print(" ");
+
+    if (brother.carriagePosition() == LEFT_OUTSIDE) Serial.print("<");
+    else if (brother.carriagePosition() == RIGHT_OUTSIDE) Serial.print(">");
+    else Serial.print("_");
+    Serial.print("\t");
+
     if (brother.isAtLeftMark()) Serial.print(" at L");
     else if (brother.isAtRightMark()) Serial.print(" at R");
     else Serial.print("     ");
