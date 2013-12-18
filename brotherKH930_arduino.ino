fbcc1ea2707573c52ab213a0915e553647bdf85b
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-18 22:44:43 +0100

    Get it to compile again.

--- brotherKH930_arduino.ino
+++ brotherKH930_arduino.ino
@@ -1,10 +1,5 @@
 #include "brotherKH930.h"
 
-void * operator new(size_t size);
-void * operator new(size_t size) {
-   return malloc(size);
-}
-
 PinSetup pins = kniticV2Pins();
 BrotherKH930 brother(pins, onChange, 0);
 
