ed261e91cec700984659392fb350c5d98c05ad5c
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-26 18:45:41 +0100

    Give feedback if needle pattern is invalid or incomplete.

--- brotherKH930_arduino.ino
+++ brotherKH930_arduino.ino
@@ -84,14 +84,32 @@ void readInput() {
 void handleLine(byte* buffer, int len) {
   if (len == 0) return;
 
-  if (len > 4 && buffer[0] == '$' && buffer[1] == '\t' && buffer[2] == '>' && buffer[3] == '\t') {
-    for (int i=4,n=0; i<len; i++, n++) {
+  if (len > 3 && buffer[0] == '$' && buffer[1] == '\t' && buffer[2] == '>' && buffer[3] == '\t') {
+    if (len < 204) {
+      Serial.print("$\t!\tInvalid needle pattern (too short): ");
+      Serial.write(&buffer[4], len-4);
+      Serial.println();
+      return Serial.flush();
+    }
+    for (int i=4,n=0; n<200; i++, n++) {
       if (buffer[i] == '1') brother.needle(n, true);
       else if (buffer[i] == '0') brother.needle(n, false);
+      else if (buffer[i] == '\t') {
+        Serial.print("$\t!\tInvalid needle pattern (termination): ");
+        Serial.write(&buffer[4], len-4);
+        Serial.println();
+        return Serial.flush();
+      } else {
+        Serial.print("$\t!\tInvalid needle pattern (invalid char): ");
+        Serial.write(&buffer[4], len-4);
+        Serial.println();
+        Serial.flush();
+        return Serial.flush();
+      }
     }
     Serial.print("$\t<\t");
     Serial.write(&buffer[4], len-4);
-    Serial.println('\n');
+    Serial.println();
     Serial.flush();
   } else {
     Serial.print("*\tInvalid input: ");
